02891ad5d790caa06057cafa49484bd59f9c8513
Adnan Bhanji <bhanjiadnan786@gmail.com>
2024-04-06 13:31:04 +0200

    update

--- helper/openai_api.py
+++ helper/openai_api.py
@@ -1,35 +1,52 @@
-import os
-
-
 import openai
-# from dotenv import load_dotenv
-# load_dotenv()
+import os
+import logging
 
 openai.api_key = os.getenv('OPENAI_API_KEY')
+latest_response = ""
 
 
 def chat_complition(prompt: str) -> dict:
     '''
-    Call Openai API for text completion
+    Call OpenAI API for text completion
     Parameters:
         - prompt: user query (str)
     Returns:
         - dict
     '''
     try:
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=[
-                {'role': 'system', 'content': 'You are a helpful assistant.'},
-                {'role': 'user', 'content': prompt},
-            ]
+        # Script providing context for the Assistant's responses
+        script = """You are a helpful assistant knowledgeable about farming practices, 
+        designed to collect detailed reports from farmers about their farm conditions. 
+        Please ensure you collect accurate information on the farm's name, location, size and
+        store the information in a python format hashmap and return it to the user as soon as you get all this info."""
+        
+        messages = [
+            {"role": "system", "content": script},
+            {"role": "user", "content": prompt}
+        ]
+
+        chat_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
         )
+        response = chat_response.choices[0].message['content']
+        logging.info(f"Answer reeived: {response}")
+        # print(response)
+        
+        # Saving response to a variable
+        latest_response = response
+        # print(latest_response)
         return {
             'status': 1,
-            'response': response['choices'][0]['message']['content']
+            'response': response,
+            'latest_response': latest_response
         }
-    except:
+    except Exception as e:
+        logging.error(f"Error communicating with ChatGPT: {str(e)}")
         return {
             'status': 0,
-            'response': ''
+            'response': '',
+            'latest_response': ''
         }
+    
--- src/app.py
+++ src/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify
 import sys
 import os
+import re
+import json
+
 
 # Adding the 'src' directory to the Python path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'helper')))
@@ -8,22 +11,24 @@ from openai_api import chat_complition
 from twilio_api import send_message
 
 app = Flask(__name__)
+final_response = ""
+hashmap = {}
 
-
-@app.route('/')
-def home():
-    return jsonify(
-        {
-            'status': 'OK',
-            'wehook_url': 'BASEURL/twilio/receiveMessage',
-            'message': 'The webhook is ready.',
-            'video_url': 'https://youtu.be/y9NRLnPXsb0'
-        }
-    )
+# @app.route('/')
+# def home():
+#     return jsonify(
+#         {
+#             'status': 'OK',
+#             'wehook_url': 'BASEURL/twilio/receiveMessage',
+#             'message': 'The webhook is ready.',
+#             'video_url': 'https://youtu.be/y9NRLnPXsb0'
+#         }
+#     )
 
 
 @app.route('/twilio/receiveMessage', methods=['POST'])
 def receiveMessage():
+    global hashmap
     try:
         # Extract incomng parameters from Twilio
         message = request.form['Body']
@@ -33,6 +38,28 @@ def receiveMessage():
         result = chat_complition(message)
         if result['status'] == 1:
             send_message(sender_id, result['response'])
+            print(1)
+        if extract_hashmap(result['response']):
+            final_response = extract_hashmap(result['response'])
+            # Convert the string to a hashmap (dictionary)
+            hashmap = json.loads(extract_hashmap(result['response']))
+            send_message(sender_id, final_response)
     except:
         pass
+
+    print(hashmap)
+    print(type(hashmap))
     return 'OK', 200
+
+def extract_hashmap(text):
+    # Regex pattern to match the hashmap
+    pattern = r'\{(?:\s*".*?"\s*:\s*".*?",?)*\s*\}'
+    
+    # Search for the pattern in the text
+    match = re.search(pattern, text)
+    
+    if match:
+        return match.group()
+    else:
+        return None
+    
